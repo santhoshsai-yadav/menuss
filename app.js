b70d20a01e97e67b3435a4b6012c1c0717194b19
saisanthosh yadav <141811579+santhoshsai-yadav@users.noreply.github.com>
2023-11-27 14:57:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// let changeIcon= function(icon){
+//     icon.classList.toggle('fa-times')
+// }
+
+changeIcon=(icon) => icon.classList.toggle('fa-times')
+
+// const toggle=document.getElementById('toggleDark');
+// const body=document.querySelector('body');
+
+
+// toggle.addEventListener('click',function(){
+//     this.classList.toggle('fa-moon');
+//     if(this.classList.toggle('fa-sun')){
+//         body.style.background='white';
+//         body.style.color='black';
+//         body.style.transition='2s';
+//     }else{
+//         body.style.background='black';
+//         body.style.color='white';
+//         body.style.transition='2s'; 
+//     }
+// })
